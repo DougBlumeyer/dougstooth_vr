f403bc013c9286581d9a5020b7554d5ff773735d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-03-13 11:34:32 -0700

    refine screen cubes

--- js/load_screen_cubes.js
+++ js/load_screen_cubes.js
@@ -1,15 +1,15 @@
 function loadScreenCubes() {
   var geometry = new THREE.BoxGeometry(5, 5, 0.1);
-  var material = new THREE.MeshBasicMaterial( { color: 0x666666, map: renderedTexture } );
+  var material = new THREE.MeshBasicMaterial({color: 0x666666, map: renderedTexture});
   mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(0, 0, -2.25);
   window.screenCube = mesh;
   scene.add(mesh);
 
   var geometry = new THREE.BoxGeometry(5, 5, 0.1);
-  var material = new THREE.MeshBasicMaterial( { color: 0x666666, map: renderedTexture } );
+  var material = new THREE.MeshBasicMaterial({color: 0x666666, map: renderedTexture});
   mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 0, -7.25);
+  mesh.position.set(0, 0, -7.75);
   window.secondScreenCube = mesh;
   secondScene.add(mesh);
 }
