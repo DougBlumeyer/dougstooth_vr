afa792a793ccad5a1d77b77fe1abf22581523a5b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-03-13 09:47:32 -0700

    make screen more screen-shaped

--- js/load_screen_cube.js
+++ js/load_screen_cube.js
@@ -1,8 +1,8 @@
 function loadScreenCube() {
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var geometry = new THREE.BoxGeometry(5, 5, 0.1);
   var material = new THREE.MeshBasicMaterial( { color: 0x666666, map: renderedTexture } );
   mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 0, -3);
+  mesh.position.set(0, 0, -2.5);
   window.screenCube = mesh;
   scene.add(mesh);
 }
