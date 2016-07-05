71ad28ffbd8d6fef2cc561695d17f697bcee24f0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-07-04 21:32:53 -0700

    mute bug with render to texture

--- js/load_screen_cubes.js
+++ js/load_screen_cubes.js
@@ -1,13 +1,13 @@
 function loadScreenCubes() {
   var geometry = new THREE.BoxGeometry(5, 5, 0.1);
-  var material = new THREE.MeshBasicMaterial({color: 0x666666, map: renderedTexture});
+  var material = new THREE.MeshBasicMaterial({color: 0x666666});//, map: renderedTexture});
   mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(0, 0, -8);
   window.screenCube = mesh;
   scene.add(mesh);
 
   var geometry = new THREE.BoxGeometry(5, 5, 0.1);
-  var material = new THREE.MeshBasicMaterial({color: 0x666666, map: renderedTexture});
+  var material = new THREE.MeshBasicMaterial({color: 0x666666});//, map: renderedTexture});
   mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(0, 0, -7);
   window.secondScreenCube = mesh;
